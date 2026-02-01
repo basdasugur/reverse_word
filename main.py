c750f26e5a9b702438c12101e46d464c0a12f5c5
Uğur Başdaş <99135103+basdasugur@users.noreply.github.com>
2026-02-01 21:37:00 +0300

    Add word reversal functionality in main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# the reverse of the entered word
+
+# Enter a word
+word = input("Enter a word : ")
+print(f"Your word : {word}")
+
+
+# Reverse a word
+reverse_wrod = word[::-1]
+print(f"Recover your word : {reverse_wrod}")
+
